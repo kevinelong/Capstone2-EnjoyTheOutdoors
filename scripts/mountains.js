e90f920acc7532f88b1d53c70ddd0c863faf63ea
kevinelong <kevinelong@gmail.com>
2023-11-17 13:33:33 -0800

    return all matches from search text as you type

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", e => {
     // SECOND MAKE SURE DATA IS LOADED LIKE THIS
     //document.body.innerHTML += mountainsArray.length; //We should se 48 in the show preview
 
+
     // FOURTH LOOP THROUGH ALL DATA AND ADD A NEW OPTION FOR EACH TO THE SELECT
     mountainsArray.forEach(mountain => { //USE FOR EACH METHOD OF THE DATA TO GET 1 AT A TIME IN ARROW FUNCTION
         const option = document.createElement("option"); //CREATE BLANK OPTION ELEMENT
@@ -9,14 +10,26 @@ document.addEventListener("DOMContentLoaded", e => {
         list.appendChild(option); // PLACE OPTION IN THE SELECT LIST
     }); //END FOR EACH
 
-    // SIXTH - LISTEN FOR LIST CHANGE SELECTION
-    list.addEventListener("change", event => {
-        let mountain = mountainsArray.find(m => m.name == event.target.value); //FIND DATA
-        results.innerHTML = `<h1>${mountain.name}</h1>`; //SHOW RESULTS
+    function draw(mountain){
+        results.innerHTML += `<hr><h1>${mountain.name}</h1>`; //SHOW RESULTS
         results.innerHTML += mountain.desc;
         results.innerHTML += `<h3>Elevation: ${mountain.elevation} feet</h3>`;
         results.innerHTML += `<h3>Effort: ${mountain.effort}</h3>`;
         results.innerHTML += `<img src="./images/${mountain.img}">`;
+    }
+    // SIXTH - LISTEN FOR LIST CHANGE SELECTION
+    list.addEventListener("change", event => {
+        results.innerHTML = "";
+        let mountain = mountainsArray.find(m => m.name == event.target.value); //FIND DATA
+        draw(mountain);
     }); //END LISTENER
+
+    searchInput.addEventListener("keyup", e=>{
+        results.innerHTML = "";
+        // let mountain = mountainsArray.find(m => m.desc.toLowerCase().includes(searchInput.value.toLowerCase())); //FIND DATA
+        // draw(mountain);
+        let mountainArray = mountainsArray.filter(m => m.desc.toLowerCase().includes(searchInput.value.toLowerCase())); //FIND DATA
+        mountainArray.forEach(draw);
+    })
 
 }); // END LOADED
